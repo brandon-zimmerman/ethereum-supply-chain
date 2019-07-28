32ae86e95352e986120654dbbed20ad21e3df1bb
Brandon Zimmerman <b.j.zimmerman@outlook.com>
2019-07-28 17:36:30 -0500

    Initial commit

---
+++ contracts/pharmaceuticalaccesscontrol/Roles.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.4.24;
+
+/**
+ * @title Roles
+ * @dev Library for managing addresses assigned to a Role.
+ */
+library Roles {
+  struct Role {
+    mapping (address => bool) bearer;
+  }
+
+  /**
+   * @dev give an account access to this role
+   */
+  function add(Role storage role, address account) internal {
+    require(account != address(0), "the account cannot be zero");
+    require(!has(role, account), "the account already has the given role");
+
+    role.bearer[account] = true;
+  }
+
+  /**
+   * @dev remove an account's access to this role
+   */
+  function remove(Role storage role, address account) internal {
+    require(account != address(0), "the account cannot be zero");
+    require(has(role, account), "the account does not have the given role");
+
+    role.bearer[account] = false;
+  }
+
+  /**
+   * @dev check if an account has this role
+   * @return bool
+   */
+  function has(Role storage role, address account)
+    internal
+    view
+    returns (bool)
+  {
+    require(account != address(0), "the account cannot be zero");
+    return role.bearer[account];
+  }
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,14 @@
+// migrating the appropriate contracts
+var ManufacturerRole = artifacts.require("./ManufacturerRole.sol");
+var DistributorRole = artifacts.require("./DistributorRole.sol");
+var RetailerRole = artifacts.require("./RetailerRole.sol");
+var ConsumerRole = artifacts.require("./ConsumerRole.sol");
+var SupplyChain = artifacts.require("./SupplyChain.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(ManufacturerRole);
+  deployer.deploy(DistributorRole);
+  deployer.deploy(RetailerRole);
+  deployer.deploy(ConsumerRole);
+  deployer.deploy(SupplyChain);
+};
